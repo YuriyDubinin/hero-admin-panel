73682c14f1b35bf64cab4d26a56b679c725c330e
Yura Dubinin <yuradubinin100@gmail.com>
2022-06-16 18:16:29 +0700

    Library redux-thunk connected to the project
    * Now we are use ReduxThunk middleware
    
    * Added 2 new action creators: fetchHeroes & fetchFilters
    
    * HeroesList & HeroesFilters component now use new action creators to get heroes & filters respectively

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import classNames from "classnames";
 
 import { useHttp } from "../../hooks/http.hook";
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from "../../actions";
+import { fetchFilters, activeFilterChanged } from "../../actions";
 import Spinner from "../spinner/Spinner";
 
 const HeroesFilters = () => {
@@ -12,12 +12,7 @@ const HeroesFilters = () => {
     const { request } = useHttp();
 
     useEffect(() => {
-        dispatch(filtersFetching());
-        request("http://localhost:3001/filters")
-            .then((data) => {
-                dispatch(filtersFetched(data));
-            })
-            .catch(() => dispatch(filtersFetchingError()));
+        dispatch(fetchFilters(request));
 
         // eslint-disable-next-line
     }, []);
